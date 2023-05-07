4573a53c4b78541920184a88027ba4186dc3b8d8
Letalandroid <carlossoncra@gmail.com>
2023-05-07 17:20:29 -0500

    Init with project

--- rutas.ts
+++ rutas.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const ruta = express.Router();
 
-ruta.post('/', (req: any, res: any) => {
+ruta.post('/', (req, res) => {
 	const transporter = nodemailer.createTransport({
 		host: 'smtp.gmail.com',
 		port: 587,
